a15cd09d562c88d2089a4638e0a06697df5a0d4f
reva <reva.erica24@gmail.com>
2025-10-13 04:49:44 +0700

    Update config _4-5.sh

--- config _4-5.sh
+++ config _4-5.sh
@@ -1,51 +1,60 @@
-#Tirion
-apt-get update -y
-apt-get install bind9 -y
+apt-get update
+apt-get install ifupdown -y
 
-cat > /etc/bind/named.conf.local <<EOF
-zone "K49.com" {
+# Tirion (ns1 / master)
+apt-get update
+apt-get install ifupdown -y
+
+apt-get install bind9 -y
+ln -s /etc/init.d/named /etc/init.d/bind9
+
+mkdir -p /etc/bind/K49
+
+cat > /etc/bind/named.conf.local <<'EOF'
+zone "jarkomK49.com" {
     type master;
     notify yes;
-    also-notify { 10.88.3.4; };
+    also-notify { 10.88.3.4; };      // IP Valmar (ns2)
     allow-transfer { 10.88.3.4; };
-    file "/etc/bind/K49/K49.com";
+    file "/etc/bind/K49/jarkomK49.com";
 };
 EOF
 
-mkdir -p /etc/bind/K49
-cat > /etc/bind/K49/K49.com <<EOF
+cat > /etc/bind/K49/jarkomK49.com<<'EOF'
 $TTL 604800
-@       IN SOA ns1.K49.com. root.K49.com. (
-                            2025101202
-                			604800
-                			86400
-                			2419200
-                			604800 )
-@       	IN 		NS 	ns1.K49.com.
-@       	IN 		NS 	ns2.K49.com.
-@       	IN 		A 	10.88.3.2
-ns1     	IN 		A 	10.88.3.3
-ns2     	IN 		A 	10.88.3.4
-Earendil 	IN 		A 	10.88.1.2
-Elwing   	IN 		A 	10.88.1.3
-Cidran   	IN 		A 	10.88.2.2
-Elrond   	IN 		A 	10.88.2.3
-Maglor   	IN 		A	10.88.2.4
-Sirion   	IN 		A 	10.88.3.2
-Lindon   	IN 		A 	10.88.3.5
-Vingilot 	IN 		A 	10.88.3.6
-www      	IN 	CNAME Sirion.K49.com.
-static   	IN 	CNAME Lindon.K49.com.
-app      	IN 	CNAME Vingilot.K49.com.
+@       IN SOA ns1.jarkomK49.com root.jarkomK49.com (
+            2025101301 ; Serial YYYYMMDDXX
+            604800     ; Refresh 1 minggu
+            86400      ; Retry 1 hari
+            2419200    ; Expire 4 minggu
+            604800 )   ; Negative Cache TTL
+
+@       IN NS ns1.jarkomK49.com
+@       IN NS ns2.jarkomK49.com
+
+; Records A
+ns1     IN A 10.88.3.3
+ns2     IN A 10.88.3.4
+Earendil IN A 10.88.1.2
+Elwing   IN A 10.88.1.3
+Cidran   IN A 10.88.2.2
+Elrond   IN A 10.88.2.3
+Maglor   IN A 10.88.2.4
+Sirion   IN A 10.88.3.2
+Lindon   IN A 10.88.3.5
+Vingilot IN A 10.88.3.6
+
+; Alias / CNAME
+www      IN CNAME Sirion.jarkomK49.com
+static   IN CNAME Lindon.jarkomK49.com
+app      IN CNAME Vingilot.jarkomK49.com
 EOF
 
-cat > /etc/bind/named.conf.options <<EOF
+cat > /etc/bind/named.conf.options <<'EOF'
 options {
     directory "/var/cache/bind";
     dnssec-validation no;
-    forwarders {
-        192.168.122.1;
-    };
+    forwarders { 192.168.122.1; };
     allow-query { any; };
     auth-nxdomain no;
     listen-on-v6 { any; };
@@ -54,21 +63,31 @@ EOF
 
 service bind9 restart
 
-
-#Valmar
-apt-get update -y
+# Valmar (ns2 / slave)
+apt-get update
+apt-get install ifupdown -y
 apt-get install bind9 -y
+ln -s /etc/init.d/named /etc/init.d/bind9
 
 mkdir -p /etc/bind/K49
-cat > /etc/bind/named.conf.local <<EOF
-zone "K49.com" {
+
+cat > /etc/bind/named.conf.local <<'EOF'
+zone "jarkomK49.com" {
     type slave;
-    masters { 10.88.3.3; };
-    file "/etc/bind/K49/K49.com";
+    masters { 10.88.3.3; };           // IP Tirion / ns1
+    file "/etc/bind/K49/jarkomK49.com";
+};
+EOF
+
+cat > /etc/bind/named.conf.options <<'EOF'
+options {
+    directory "/var/cache/bind";
+    dnssec-validation no;
+    forwarders { 192.168.122.1; };
+    allow-query { any; };
+    auth-nxdomain no;
+    listen-on-v6 { any; };
 };
 EOF
 
 service bind9 restart
-
-# di semua console
-echo -e "nameserver 10.88.3.3\nnameserver 10.88.3.4\nnameserver 192.168.122.1" > /etc/resolv.conf
