bc4fef181f8e66118216383832e22c3792425ef6
reva <reva.erica24@gmail.com>
2025-10-13 21:33:31 +0700

    Update Soal_5.sh

--- Soal_5.sh
+++ Soal_5.sh
@@ -30,12 +30,13 @@ nameserver 10.88.3.4   # ns2.jarkomK49.com
 nameserver 192.168.122.1  # fallback Internet
 EOF
 
-cat > /etc/resolv.conf <<EOF_DNS
+# ke host non router
+cat > /etc/resolv.conf
 nameserver 10.88.3.3
 nameserver 10.88.3.4
 nameserver 192.168.122.1
 search jarkomK49.com
-EOF_DNS
+EOF
 
 # Verif DNS internal
 # Cek apex domain
