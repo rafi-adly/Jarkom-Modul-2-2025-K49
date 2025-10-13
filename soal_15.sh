f0efb539f85ebefa244caa810dc4bf4628c93ae3
reva <reva.erica24@gmail.com>
2025-10-13 23:43:15 +0700

    Update soal_15.sh

--- soal_15.sh
+++ soal_15.sh
@@ -1,1 +1,5 @@
+echo "=== HASIL UJI /app/ (Vingilot) ==="
+ab -n 500 -c 10 http://www.jarkomK49.com/app/
 
+echo "=== HASIL UJI /static/ (Lindon) ==="
+ab -n 500 -c 10 http://www.jarkomK49.com/static/
