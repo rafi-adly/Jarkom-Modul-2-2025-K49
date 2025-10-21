18fb0d7fbaa1ff02373a9e8ee74bb7a5c75e998f
reva <reva.erica24@gmail.com>
2025-10-21 18:10:19 +0700

    Enhance soal_13.sh with Nginx setup instructions
    Add Nginx configuration commands to soal_13.sh

--- soal_13.sh
+++ soal_13.sh
@@ -1,1 +1,14 @@
+# Vingilot
+nano /etc/nginx/sites-available/app.conf
 
+# tambahkan ini ke dalam server { ... }
+access_log /var/log/nginx/access.log main;
+
+nginx -t
+sever nginx stop
+server nginx start
+
+tail /var/log/nginx/access.log
+
+# Di client
+curl http://www.jarkomK49.com/app/
